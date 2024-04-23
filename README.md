f80af719d1d532481f247104276cddaa53eaf755
AbdulQohar1 <adebayoabdulqohar1@gmail.com>
2024-04-23 22:50:19 +0100

    telegram bot

---
+++ README.md
@@ -0,0 +1,20 @@
+## Install Dependencies and packages
+
+## Create the Bot and obtain bot token
+Search for BotFather on your telegram app
+- Follow the instructions in the chat with the BotFather to create a new bot.
+- Once the bot is created, an API authentication token that grants to the bot's functionality.
+
+## Create a file in the Project's directory
+- Import the node-telegram-bot-api
+- Pass the API token (provided by the BotFather) to initialize the bot, allowing to listen for and receive message and requests.
+- Add event listener to handle incoming requests and messsages.
+- Implement logic to respond to user requests and messages within the listener.
+
+## Run your bot
+
+
+
+
+ https://4f0e-102-88-69-34.ngrok-free.app
+
---
+++ bot.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const { reset } = require('nodemon');
+const app = express();
+
+
+// Parsing request application requests/ json objects
+// app.use(bodyParser.json());
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: true,
+//   })
+// );
+app.use(express.json());
+app.post("*", async (req , res) => {
+  console.log(req.body);
+  
+  res.send('Hello post')
+
+  // const { message } = req.body;
+  // console.log('Request body:', req.body);
+
+  // if (!message || message.text.toLowerCase().indexOf("macro") < 0) {
+  //   return res.end();
+  // };
+  
+  // axios.post(
+  //   "https://api.telegram.org/bot6942405195:AAHq_sDaahTTNJHdzGKwfBHmYfY2b9psZ6A/sendMessage",
+  //   {
+  //     chat_id: message.chat.id,
+  //     text: "Unknown"
+  //   }
+  // )
+  // .then(( response ) => {
+  //   console.log('Message posted!!');
+
+  //   res.end("Ok")
+  // })
+  // .catch( (err) => {
+  //   console.log("Error :", err);
+  //   res.end("Error :" + err);
+  // })
+});
+
+app.get("*", async (req , res) => {
+  res.send('Hello get ')
+})
+
+
+const port = process.env.PORT || 3000;
+const start = async () => {
+  try {
+    app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+  } catch (error) {
+    console.log(error)
+  }
+};
+
+start();
+
+// https://c355-105-112-212-247.ngrok-free.app -> http://localhost:4040 
+/*
+https://api.telegram.org/bot${MY_TOKEN}/${method}
+*/
